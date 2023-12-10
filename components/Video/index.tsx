e6637b86aa53cda7db799b0c93019b8055e44631
mayank2808sharma <mayank2808sharma@gmail.com>
2023-12-10 09:07:54 +0530

    completing the video

--- components/Video/index.tsx
+++ components/Video/index.tsx
@@ -23,7 +23,7 @@ const Video = () => {
             >
               <div className="relative aspect-[77/40] items-center justify-center">
                 <div className="absolute right-0 top-0 flex h-full w-full items-center justify-center">
-                <iframe width="700" height="515" src="https://www.youtube.com/embed/27YUW4SA6oE?si=c53Kz7G60Rr0kBtl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <iframe width="700" height="515" src="https://www.youtube.com/embed/27YUW4SA6oE?si=c53Kz7G60Rr0kBtl" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
                 </div>
               </div>
             </div>
