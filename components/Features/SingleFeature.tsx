bf641b4a14cec2bae08224a1ffe962c31785eb28
kairveeehh <jampu741@gmail.com>
2024-05-10 23:32:27 +0530

    better UI

--- components/Features/SingleFeature.tsx
+++ components/Features/SingleFeature.tsx
@@ -3,9 +3,11 @@ import { Feature } from "@/types/feature";
 const SingleFeature = ({ feature }: { feature: Feature }) => {
   const { icon, title, paragraph } = feature;
   return (
-    <div className="w-full">
+    <div className="w-full border border-primary text-primary inline-700 rounded-md transform transition-all duration-300 hover:scale-108 p-4 cursor-pointer  ">
+    
       <div className="wow fadeInUp" data-wow-delay=".15s">
-        <div className="mb-10 flex h-[70px] w-[70px] items-center justify-center rounded-md bg-primary bg-opacity-10 text-primary">
+      <div className="mb-10 flex h-[70px] w-[70px] items-center justify-center rounded-md  bg-opacity-10 text-primary ">
+  
           {icon}
         </div>
         <h3 className="mb-5 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
