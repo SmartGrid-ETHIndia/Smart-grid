d12b52a4a5fdf3304cfe745fe61b810a733d0abe
ENZOMOTIVE <aayushmanbhabapadhy@gmail.com>
2024-05-11 13:03:05 +0530

    Toggle Button

--- components/Header/ThemeToggler.tsx
+++ components/Header/ThemeToggler.tsx
@@ -1,11 +1,28 @@
+import { useState } from "react";
 import { useTheme } from "next-themes";
 
 const ThemeToggler = () => {
   const { theme, setTheme } = useTheme();
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleToggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+
   return (
-    <button aria-label='theme toggler'
-      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-      className="flex items-center justify-center text-black rounded-full cursor-pointer bg-gray-2 dark:bg-dark-bg h-9 w-9 dark:text-white md:h-14 md:w-14"
+    <button
+      aria-label="theme toggler"
+      onClick={handleToggleTheme}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      className={`flex items-center justify-center text-black rounded-full cursor-pointer bg-gray-2 dark:bg-dark-bg h-9 w-9 dark:text-white md:h-14 md:w-14 ${
+        isHovered ? "hover:bg-gray-200" : ""
+      }`}
+      style={{
+        transition: "background-color 0.3s ease", // Smooth transition
+        boxShadow: "0px 2px 4px rgba(0, 0, 100, 0.6)"
+      }}
     >
       <svg
         viewBox="0 0 23 23"
