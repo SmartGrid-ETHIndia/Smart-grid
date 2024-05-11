e52e9e50c5838b70adb0dbf8cb3bb5fa948afeb5
ENZOMOTIVE <aayushmanbhabapadhy@gmail.com>
2024-05-11 13:14:53 +0530

    final commit

--- components/Header/ThemeToggler.tsx
+++ components/Header/ThemeToggler.tsx
@@ -20,6 +20,7 @@ const ThemeToggler = () => {
         isHovered ? "hover:bg-gray-200" : ""
       }`}
       style={{
+        marginLeft: "10px",
         transition: "background-color 0.3s ease", // Smooth transition
         boxShadow: "0px 2px 4px rgba(0, 0, 100, 0.6)"
       }}
